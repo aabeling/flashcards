186c7874362f7a9443756bdfe862354808fb6dd6
Achim Abeling <achimabeling@web.de>
2019-08-18 10:11:05 +0200

    US-Staaten mit Positionskarten und Hauptstädten

--- check.sh
+++ check.sh
@@ -5,8 +5,12 @@
 #
 # each line must contain a tab character
 #
-# arguments: <filename>
+# arguments: <filename> <columns>
 #
+
+COLUMNS=$2
+echo "In jeder Zeile werden $COLUMNS Spalten erwartet"
+TABS=`expr $COLUMNS - 1`
 
 function validate() {
 
@@ -16,7 +20,7 @@ function validate() {
   fi
 
   count=`echo "$1" | fold -1 | grep -c $'\t'`
-  if [[ $count != 1 ]]; then
+  if [[ $count != $TABS ]]; then
     echo "zu wenig Tabs: $1"
   fi
 }
